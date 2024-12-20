8aa68ba42594b3a99217952a4f30f2a691d3e627
EMIL NIKOLAEV DIMITROV <nikolaev.emil@gmail.com>
2024-12-20 17:19:08 +0100

    barcode canner start

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.royousa.scanner',
   appName: 'royoapp-scanner',
-  webDir: 'dist/royoapp-scanner/browser'
+  webDir: 'dist/royoapp-scanner/browser',
+  server: {
+    url: "http://192.168.1.149:4200",
+    cleartext: true
+  },
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerCameraDirection, CapacitorBarcodeScannerOptions, CapacitorBarcodeScannerScanOrientation, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     // RouterOutlet,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'royoapp-scanner';
+
+
+  startScan = async () => {
+    const options: CapacitorBarcodeScannerOptions = {
+      cameraDirection: CapacitorBarcodeScannerCameraDirection.BACK,
+      scanOrientation: CapacitorBarcodeScannerScanOrientation.PORTRAIT,
+      hint: CapacitorBarcodeScannerTypeHint.ALL,
+      scanButton: false,
+      scanText: 'Please scan the barcode',
+    }
+    CapacitorBarcodeScanner.scanBarcode(options).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
 }
